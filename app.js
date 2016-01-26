db1b0c134cacd37fc0cc962e6cec213eac15ab35
stefanstokic <stefan@stefanstokic.com>
2016-01-26 13:37:02 -0600

    Half-working everythigng

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
 var session = require('express-session')
+var bcrypt  = require('bcrypt-nodejs');
+var LocalStrategy   = require('passport-local').Strategy;
 
 
-mongoose.connect('mongodb://admin:lowercase@ds047325.mongolab.com:47325/lowercasejobs');
+// mongoose.connect('mongodb://admin:lowercase@ds047325.mongolab.com:47325/lowercasejobs');
+mongoose.connect('mongodb://localhost:27017');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
@@ -26,23 +28,112 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+var UserSchema = new mongoose.Schema ({
+  email: String,
+  password: String
 
-passport.use(new Strategy({
-    consumerKey: 'jAd1T9NxbjnkTEIhlIYJvtfSO',
-    consumerSecret: 'SXUKm48tASfDkO6MS5R69dNMAITMtSQtCvcQWPSzJ3swWz5phA',
-    callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
-  },
-  function(token, tokenSecret, profile, cb) {
-    return cb(null, profile);
-  }));
+});
+
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+
+var User = mongoose.model('User', UserSchema);
+
+
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+       done(null, user.id);
+   });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+   // used to deserialize the user
+   passport.deserializeUser(function(id, done) {
+       User.findById(id, function(err, user) {
+           done(err, user);
+       });
+   });
+
+
+   passport.use('local-signup', new LocalStrategy({
+          // by default, local strategy uses username and password, we will override with email
+          usernameField : 'email',
+          passwordField : 'password',
+          passReqToCallback : true // allows us to pass back the entire request to the callback
+      },
+      function(req, email, password, done) {
+
+          // asynchronous
+          // User.findOne wont fire unless data is sent back
+          //process.nextTick(function() {
+
+          // find a user whose email is the same as the forms email
+          // we are checking to see if the user trying to login already exists
+          User.findOne({ 'local.email' :  email }, function(err, user) {
+              // if there are any errors, return the error
+              if (err)
+
+              return done(err);
+              // check to see if theres already a user with that email
+              if (user) {
+              } else {
+                // if there is no user with that email
+                // create the user
+                var newUser = new User();
+
+                  // set the user's local credentials
+                  newUser.email = email;
+                  newUser.password = newUser.generateHash(password);
+
+                  // save the user
+                  newUser.save(function(err) {
+                      if (err)
+                          throw err;
+                      return done(null, newUser);
+                  });
+              }
+
+          });
+
+      //    });
+
+      }));
+
+      passport.use('local-login', new LocalStrategy({
+              // by default, local strategy uses username and password, we will override with email
+              usernameField : 'email',
+              passwordField : 'password',
+              passReqToCallback : true
+          },
+          function(req, res, email, password, done) { // callback with email and password from our form
+              // find a user whose email is the same as the forms email
+              // we are checking to see if the user trying to login already exists
+
+              User.findOne({ email :  email }, function(err, user, done) {
+                  // if there are any errors, return the error before anything else
+                  if (err)
+                      return done(err);
+
+                  // if no user is found, return the message
+                  if (!user)
+                      console.log('No User Found, Please Sign Up');
+
+
+                  // if the user is found but the password is wrong
+                  if (bcrypt.compareSync(password, user.password) === false)
+                      console.log('Wrong Password for Login');
+
+                  // all is well, return successful user
+                  return done(null, user);
+
+                res.send(json);
+              });
+
+          }));
 
 
 var PostSchema = new mongoose.Schema({
@@ -50,7 +141,6 @@ var PostSchema = new mongoose.Schema({
   description: String,
   company: String,
   moreinfo: String
-
 });
 
 var Post = mongoose.model('Post', PostSchema);
@@ -62,6 +152,8 @@ app.get('/api/posts', function(req, res) {
     res.json(post)
   });
 });
+
+
 /*
 app.post('/api/submit', function(req, res) {
 
@@ -77,6 +169,25 @@ app.post('/api/submit', function(req, res) {
   });
 });
 */
+
+app.post('/api/signup', passport.authenticate('local-signup', {
+      successRedirect : '/profile', // redirect to the secure profile section
+      failureRedirect : '/signup' // redirect back to the signup page if there is an error
+  }));
+
+app.post('/api/login', passport.authenticate('local-login', {
+       successRedirect : '/profile', // redirect to the secure profile section
+       failureRedirect : '/login', // redirect back to the signup page if there is an error
+ }));
+
+app.get('/api/users', function(req, res){
+  User.find(function(err, user) {
+    if (err)
+      res.send(err)
+    res.json(user)
+  });
+});
+
 app.get('/', function(req, res) {
   res.sendfile('./public/index.html');
 });
@@ -85,18 +196,24 @@ app.get('/submit', function(req, res) {
   res.sendfile('./public/submit.html');
 });
 
-app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/login', function(req, res) {
+  res.sendfile('.public/login.html');
+});
 
-app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-  failureRedirect: '/fail',
-  successRedirect: '/'
-}));
+app.get('/signup', function(req, res){
+  res.sendfile('./public/signup.html')
+});
+
+app.get('/logout', function(req, res) {
+       req.logout();
+       res.redirect('/');
+   });
+
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()); {
     return next();
   }
-
 
   res.redirect('/');
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var concat = require("gulp-concat");
+var nodemon = require('gulp-nodemon');
+
 
 gulp.task("sass", function() {
   gulp.src('./public/sass/**/*.scss')
@@ -13,8 +15,17 @@ gulp.task("watch", function() {
   gulp.watch("./public/sass/**/*.scss", ["sass"]);
 })
 
+gulp.task('start', function () {
+  nodemon({
+    script: 'app.js'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
+
 gulp.task("default",
 [
   "sass",
-  "watch"
-])
+  "watch",
+  "start"
+]
+)
